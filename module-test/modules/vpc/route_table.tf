5fe668e3cec5912e1a97b82568c72abc8514b3fc
yrpark <yrpark@ensmart.co.kr>
2026-09-09 15:07:38 +0900

    NAT 라우팅 경로 관련 코드 추가

--- module-test/modules/vpc/route_table.tf
+++ module-test/modules/vpc/route_table.tf
@@ -54,15 +54,44 @@ resource "aws_route_table_association" "this" {
 ############################ Add Routing into Route Table ############################
 ## IGW 경로
 locals {
-    public_rt_information = {
+    igw_rt_information = {
         for k,v in local.rt_information : k => v if(strcontains(k, "pub"))
     }
 }
 
 resource "aws_route" "igw" {
-    for_each = local.public_rt_information
+    for_each = local.igw_rt_information
 
     route_table_id = aws_route_table.this[each.key].id
     destination_cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.this[0].id 
 }
+
+## NGW 라우팅
+locals {
+    ngw_rt_information = {
+        for k,v in local.rt_information : k => merge(v,{
+            # 1. NGW가 1개만 있으면 무조건 그 NGW 선택 
+            # 2. NGW가 여러개 있으면 라우팅 테이블의 AZ와 일치하는 NGW 선택
+            # 3. NGW가 없으면 null
+            nat_gateway_name = (
+                length(local.ngw_information) == 1 ? keys(local.ngw_information)[0] : 
+                length(local.ngw_information) > 1 ? try(
+                    [for ngw_k, ngw_v in local.ngw_information : ngw_k if ngw_v.az == v.az][0], null
+                ) : null 
+            )
+        }) 
+        # main.tf에 명시한 그룹만 선택
+        if ( contains(var.allow_nat_route_subnet, v.group) )
+    }
+}
+
+resource "aws_route" "ngw" {
+    for_each = local.ngw_rt_information
+
+    route_table_id = aws_route_table.this[each.key].id
+    destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this[each.value.nat_gateway_name].id
+}
+
+## 그외 사용자 지정 라우팅
