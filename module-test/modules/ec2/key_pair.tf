c1fb6d17265634ff656e7a84582779bbedbecf3f
yrpark0920 <rin3406@naver.com>
2026-09-10 14:48:07 +0900

    키페어 생성 코드 추가

---
+++ module-test/modules/ec2/key_pair.tf
@@ -0,0 +1,16 @@
+## 키 생성
+resource "tls_private_key" "this" {
+    algorithm = "RSA"
+}
+
+## 키페어 생성
+resource "aws_key_pair" "this" {
+    key_name = "${var.vpc_name}-key-pair"
+    public_key = tls_private_key.this.public_key_openssh
+}
+
+## 개인키를 PC에 저장
+resource "local_file" "this" {
+    content = tls_private_key.this.private_key_pem
+    filename = "${path.module}/${var.vpc_name}.pem"
+}
